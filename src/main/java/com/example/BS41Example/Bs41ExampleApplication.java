52e8f6cb2d121f8b270eb6aba3c159e0ae432637
marcos.corona <marcoscoronadionisio@gmail.com>
2022-06-14 13:26:44 +0200

    Initial commit

---
+++ src/main/java/com/example/BS41Example/Bs41ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.BS41Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs41ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs41ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BS41Example/Bs41ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BS41Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs41ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
